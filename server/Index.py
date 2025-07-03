c95d0544531b3a96c4576760805bdeb5232a0d58
Your Name <you@example.com>
2025-07-03 19:51:07 +0530

    after clearing the chaos

--- server/Index.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return "Welcome to the Notes App Server!"
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
